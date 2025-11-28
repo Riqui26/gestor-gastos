24487de8cd2d4652cf11e5621d274482a7910f2a
Fede2606 <friquelme2606@gmail.com>
2025-11-28 18:10:42 -0300

    LogIn - LogOut finalizado

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// PRINCIPAL
+export { default as App } from "./App.jsx";
+
+
+// COMPONENTS
+// atoms
+export * from "./components/atoms/Icono.jsx";
+
+// molecules
+export * from "./components/molecules/Btnsave.jsx";
+
+// organisms
+
+
+// templates
+export * from "./components/templates/HomeTemplate.jsx";
+export * from "./components/templates/LoginTemplate.jsx";
+
+
+//CONTEXT
+export * from "./context/AuthContent.jsx";
+
+
+// HOOKS
+export * from "./hooks/ProtectedRoute.jsx";
+
+
+// PAGES
+export * from "./pages/Home.jsx";
+export * from "./pages/Login.jsx";
+
+
+// QUERYS
+
+
+// ROUTERS
+export * from "./routers/routes.jsx";
+
+
+// STORE
+export * from "./store/AuthStore.jsx";
+
+// STYLES
+export * from "./styles/themes.jsx";
+export * from "./styles/variables.jsx";
+
+// SUPABASE
+export * from "./supabase/supabase.config.jsx";
+
+// UTILS
+
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { useAuthStore } from "../index";
+
+export function Home() {
+  const { signout } = useAuthStore();
+
+  return (
+    <Container>
+      <h1>Home</h1>
+      <button onClick={signout}>Cerrar sesión</button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
